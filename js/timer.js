0068a853083d37a9b55b9bce70dafc4e7246654b
wtmti2021 <willtmelo@yahoo.com.br>
2022-09-22 07:35:01 -0300

    Focus Timer V1.1

--- js/timer.js
+++ js/timer.js
@@ -7,8 +7,10 @@ export default function Timer({
   let timerTimeOut
   let minutes = Number(minutesDisplay.textContent)
 
-function updateDisplay(minutes, seconds) {
-  minutesDisplay.textContent = String(minutes).padStart(2, "0")
+function updateDisplay(newMinutes, seconds) {
+  newMinutes = newMinutes === undefined ? minutes : newMinutes
+  seconds = seconds === undefined ? 0 : seconds
+  minutesDisplay.textContent = String(newMinutes).padStart(2, "0")
   secondsDisplay.textContent = String(seconds).padStart(2, "0")  
 }
 
@@ -21,16 +23,18 @@ function countdown() {
   timerTimeOut = setTimeout(function() {
     let seconds =  Number(secondsDisplay.textContent)
     let minutes = Number(minutesDisplay.textContent)
+    let isFinished = minutes <= 0 && seconds <= 0
 
     updateDisplay(minutes, 0)
 
-    if (minutes <= 0) {
+   if (isFinished) {
       resetControls()
+      updateDisplay()
       return
     }
 
-    if( seconds <= 0 ) {
-      seconds = 3
+    if( seconds <= 0) {
+      seconds = 6
       --minutes
     }
 
